d045523a31213876e007b3e6fb91ae8e3d15eebd
joramcito <joramjosafatchavezdelgado@gmail.com>
2019-06-26 14:31:41 -0500

    Agregando el menu de inicio

--- app/src/main/java/com/chavez/mrjuerga/MainActivity.java
+++ app/src/main/java/com/chavez/mrjuerga/MainActivity.java
@@ -2,13 +2,27 @@ package com.chavez.mrjuerga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Login=(Button)findViewById(R.id.btnInicio);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Login= new Intent(MainActivity.this, InicioActivity.class);
+                startActivity(Login);
+            }
+        });
     }
 }
